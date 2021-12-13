394b08ef2919f8d1aa1a162ff667e2d636f460e0
Krystin McKee <mckee.km@gmail.com>
2021-12-13 14:45:50 -0700

    updating correct folder

--- simple_map/static/js/logic.js
+++ simple_map/static/js/logic.js
@@ -3,9 +3,9 @@ console.log("working");
 
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-                // ALTERNATE to using the setView() method is to modify each attribute 
+                // An alternative to using the setView() method is to modify each attribute 
                 // in the map object using the curly braces notation as follows:
                 // Create the map object with a center and zoom level.
                     // let map = L.map("mapid", {
@@ -17,32 +17,8 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
                 // This method is useful when we need to add multiple tile layers, or a background image of our map(s)
 
 
-
-// _______________________________________________
-
-                // //  BASIC MARKER: Add a marker to the map for Los Angeles, California.
-                // let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-                // // CIRCLE MARKER: Place a circle on the map at the given coordinates using the circle() function
-                    // // this function tracks radius in meters...
-                // L.circle([34.0522, -118.2437], {
-                //     radius: 100
-                //  }).addTo(map);
-
-// Alternatively, we can create a circle using the circleMarker() function. 
-    // The circleMarker() function measures the radius of the circle in pixels
- L.circleMarker([34.0522, -118.2437], {
-     radius: 300,
-     color: "black",
-     fillColor: "#ffffa1"
- }).addTo(map);
-                
-
-// ______________________________________________
-
-
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
@@ -62,5 +38,4 @@ streets.addTo(map);
             //     mapbox/dark-v10
             //     mapbox/satellite-v9
             //     mapbox/satellite-streets-v11
-
 
