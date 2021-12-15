e9b3fc808dbbf5f5a722e7c42c7bf7fb045a1064
Krystin McKee <mckee.km@gmail.com>
2021-12-15 12:59:51 -0700

    module 13.4.2

--- mapping_multiple_points/static/js/logic.js
+++ mapping_multiple_points/static/js/logic.js
@@ -1,9 +1,10 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+// CREATING THE MAP --------------------------------------------------------------------------------------------------------------------------------
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
                 // ALTERNATE to using the setView() method is to modify each attribute 
                 // in the map object using the curly braces notation as follows:
@@ -18,27 +19,53 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 
 
-// _______________________________________________
+
+
+// CREATING MARKERS ON THE MAP --------------------------------------------------------------------------------------------------------------------
 
                 // //  BASIC MARKER: Add a marker to the map for Los Angeles, California.
-                // let marker = L.marker([34.0522, -118.2437]).addTo(map);
+                        // let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
                 // // CIRCLE MARKER: Place a circle on the map at the given coordinates using the circle() function
-                    // // this function tracks radius in meters...
-                // L.circle([34.0522, -118.2437], {
-                //     radius: 100
-                //  }).addTo(map);
+                // // this function tracks radius in meters...
+                        // L.circle([34.0522, -118.2437], {
+                        //     radius: 100
+                        //  }).addTo(map);
 
-// Alternatively, we can create a circle using the circleMarker() function. 
-    // The circleMarker() function measures the radius of the circle in pixels
- L.circleMarker([34.0522, -118.2437], {
-     radius: 300,
-     color: "black",
-     fillColor: "#ffffa1"
- }).addTo(map);
+                        // //  ALTERNATIVELY: we can create a circle using the circleMarker() function. 
+                        // // The circleMarker() function measures the radius of the circle in pixels
+                                //  L.circleMarker([34.0522, -118.2437], {
+                                //      radius: 300,
+                                //      color: "black",
+                                //      fillColor: "#ffffa1"
+                                //  }).addTo(map);
+
+// MULTIPLE MARKERS: To add a marker for each location, we have to iterate through the array and add each latitude and longitude to the map.
+// replace the marker variable (which we used to map one location) with the cities variable that references the five most populous cities array 
+//(located in the new cities.js file)
+
+// Get data from cities.js
+let cityData = cities;
                 
 
-// ______________________________________________
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    // L.marker(city.location)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: "orange",
+        weight: 4 })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map)
+});
+
+
+
+
+
+
+// CREATING TILE LAYER FOR MAP --------------------------------------------------------------------------------------------------------------------
 
 
 // We create the tile layer that will be the background of our map.
